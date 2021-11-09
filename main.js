ef2e1afb3f9e957b07b7a66b89b56dbff0744af9
SaathV2 <87659400+SaathV2@users.noreply.github.com>
2021-11-09 21:07:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var name_of_the_student_array = [ ];
+
+function submit()
+{
+    var name_1 = docment.getElementById("name_of_the_student_1").value;
+    var name_2 = docment.getElementById("name_of_the_student_2").value;
+    var name_3 = docment.getElementById("name_of_the_student_3").value;
+    var name_4 = docment.getElementById("name_of_the_student_4").value;
+
+    name_of_the_student_array.push(name1);
+    name_of_the_student_array.push(name2);
+    name_of_the_student_array.push(name3);
+    name_of_the_student_array.push(name4);
+
+    console.log(name_of_the_student_array);
+
+    document.getElementById("display_name").innnerHTML = name_of_the_student_array;
+    document.getElementById("Submit_button").style.display = "none";
+    document.getElementById("sort_button").style.display = "inline-block";
+
+
+}
+
+function sorting()
+{
+    name_of_the_student_array.sort();
+    console.log(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+}
